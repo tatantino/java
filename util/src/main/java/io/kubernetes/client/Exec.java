d96a7b5ad22ad50e9dcf9d35e7e5ab4ede62a859
kgtatantino <square.08@163.com>
2018-07-10 23:53:57 +0800

    aaaaaaaaa

--- util/src/main/java/io/kubernetes/client/Exec.java
+++ util/src/main/java/io/kubernetes/client/Exec.java
@@ -11,7 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 package io.kubernetes.client;
-
+//test
 import static io.kubernetes.client.KubernetesConstants.*;
 
 import com.google.common.io.CharStreams;
